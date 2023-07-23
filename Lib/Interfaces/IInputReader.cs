3f54437a1f47a2f5cb3dbab22b834b48a7960707
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2023-07-23 20:37:12 +0200

    Adding interfaces

---
+++ Lib/Interfaces/IInputReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.Lib.Interfaces
+{
+    public interface IInputReader
+    {
+        public float ReadInput();
+        public string AnimationState { get; set; }
+        public bool IsJumping { get; set; }
+    }
+}
---
+++ Lib/Interfaces/IMoveAble.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.Lib.Interfaces
+{
+    public interface IMoveAble
+    {
+        public Vector2 Position { get; set; }
+        public IInputReader InputReader { get; set; }
+
+        public Rectangle BoundingRectangle { get; }
+
+        public bool IsOnGround { get; set; }
+    }
+}
