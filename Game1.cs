2c26512ba5ce0b63be0b89b47676fab1336b5368
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2023-07-24 21:53:59 +0200

    update

--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,11 @@
-﻿using GameDevProject.Lib.WindowCamera;
+﻿using GameDevProject.Lib.Input;
+using GameDevProject.Lib.Interfaces;
+using GameDevProject.Lib.Levels;
+using GameDevProject.Lib.WindowCamera;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
 
 namespace GameDevProject
 {
@@ -12,6 +16,9 @@ namespace GameDevProject
 
         public static int ScreenHeight { get; private set; }
         public static int ScreenWidth { get; private set; }
+
+        private Level level;
+        private IInputReader inputReader;
 
         public Game1()
         {
@@ -28,6 +35,9 @@ namespace GameDevProject
 
             ScreenHeight = _graphics.PreferredBackBufferHeight;
             ScreenWidth = _graphics.PreferredBackBufferWidth;
+
+            inputReader = new KeyboardReader();
+
             base.Initialize();
         }
 
@@ -35,7 +45,9 @@ namespace GameDevProject
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            
+            using Stream fileStream = File.OpenRead("Content/Levels/Level1.tmx");
+            level = new Level(Services, fileStream,"Forest", inputReader ,GraphicsDevice);
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,8 +55,8 @@ namespace GameDevProject
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            
 
+            level.Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -53,6 +65,8 @@ namespace GameDevProject
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin(transformMatrix: Camera.Transform);
+
+            level.Draw(_spriteBatch);
 
             _spriteBatch.End();
 
